dca5686bf41c377bcb95c518b71a05d7c5dd2414
henry-codes007 <benedicthenry724@gmail.com>
2024-10-02 10:43:25 +0100

    summit

---
+++ calculator.js
@@ -0,0 +1,40 @@
+let displayElement = document.getElementById("display")
+let currentInput = "";
+
+function appendNumber(number) {
+    if (currentInput === "0" && number!== "."){
+        currentInput = number;
+    } else {
+        currentInput += number;
+    }
+    displayElement.innerText = currentInput;
+    
+    }
+    function appendOperator(operator){
+        if (currentInput === "") return;
+        currentInput += operator;
+        displayElement.innerText = currentInput;
+
+    }
+    function deleteDigit() {
+        currentInput = currentInput.slice(0, -1);
+        displayElement.innerText = currentInput || "0";
+
+    }
+    function resetDisplay(){
+        currentInput = "";
+        displayElement.innerText = "0";
+
+
+    }
+    function checkResult(){
+        try {
+            currentInput = eval(currentInput).toString();
+            displayElement.innerText = currentInput;
+        } catch{
+            displayElement.innerText = "Sorry";
+        }
+    }
+
+
+
